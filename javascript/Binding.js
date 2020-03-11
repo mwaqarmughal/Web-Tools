5e958cd8debac781113e4122af9b2209e1694fbe
mwaqarmughal <m.waqarughal786@gmail.com>
2020-03-11 14:35:21 +0500

    Initial commit

---
+++ javascript/Binding.js
@@ -0,0 +1,12 @@
+window.onload= function () {
+    var PT=document.getElementById("Media1")
+    PT.onclick= function (){ 
+        var wa =document.getElementById("Media1").style.display ="none";
+}  
+var qq=document.getElementById("PaymentTechnologies")
+qq.onmouseenter=function() {
+    var aaa=document.getElementById("Media1").style.display="block";
+        }
+    }
+
+
